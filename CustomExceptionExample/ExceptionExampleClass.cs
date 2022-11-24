2461bef5f8c8fe2cfb9a2fc3619e6748b757e524
viktorp999 <viktorpetrovski99@gmail.com>
2022-11-24 02:23:45 +0100

    Add project files.

---
+++ CustomExceptionExample/ExceptionExampleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomExceptionExample
+{
+    public class ExceptionExampleClass : Exception
+    {
+        public string InvalidName()
+        {
+            return "Name should not contain numbers or to be emty!";
+        }
+    }
+}
---
+++ CustomExceptionExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomExceptionExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+
+            try
+            {
+                Console.WriteLine("Enter your name");
+                name = Console.ReadLine();
+
+                while (name.Any(c => char.IsDigit(c)) || String.IsNullOrEmpty(name) || String.IsNullOrWhiteSpace(name))
+                {
+                    throw new ExceptionExampleClass();
+                }
+
+                Console.WriteLine("Your name is:{0} ", name);
+            }
+
+            catch(ExceptionExampleClass ex)
+            {
+                Console.WriteLine(ex.InvalidName());
+            }
+
+            Console.Read();
+        }
+    }
+}
